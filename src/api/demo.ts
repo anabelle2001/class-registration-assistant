213ab0e0f06bc5195c944d7713e7f1a632dbe67f
Anabelle <git@anabelle.dev>
2023-09-19 08:57:48 -0400

    add demo api (connects to demo db)

---
+++ src/api/demo.ts
@@ -0,0 +1,24 @@
+import { Hono } from 'hono'
+import { pixarDB } from '../database/demo'
+
+//We have to construct queries before executing them.
+const queries = {
+    AllMovies: pixarDB.query("SELECT ID,Title FROM Movies"),
+    SpecificMovie: pixarDB.query("SELECT * FROM Movies WHERE ID = $1"),
+}
+
+//We build our API in this object. We'll decide where it's accessible from later
+//By importing it into main.ts
+export const demoAPI = new Hono();
+
+demoAPI.get('/films', (c) => {
+    return c.json(queries.AllMovies.all())
+})
+
+demoAPI.get('/films/:id', (c) => {
+    const idFromURL = c.req.param().id;
+    const movieData = queries.SpecificMovie.get(idFromURL)
+    return c.json(movieData)
+})
+
+//More Documentation: https://hono.dev/guides/examples#web-api
