9c9d4df879a3e0932a858a56a5d9ed967d80fdf5
Anabelle <git@anabelle.dev>
2023-09-19 07:16:25 -0400

    implement demo database

---
+++ src/database/demo.ts
@@ -0,0 +1,16 @@
+// This file creates a [Database][1] Object to be used in the demo api.
+// It's used to store the data used in ./api/demo.ts
+import { Database } from "bun:sqlite";
+import * as path from 'path';
+
+//Code to calcuate database path stolen from [Stackoverflow][2]
+const __dirname = new URL('.', import.meta.url).pathname;
+const dbPath = path.join(__dirname, "demo.sqlite3");
+
+
+export const pixarDB: Database = new Database(dbPath);
+
+
+// External Links: 
+// [1]: https://bun.sh/docs/api/sqlite
+// [2]: <https://stackoverflow.com/questions/73339396/how-does-nodejs-handle-relative-paths>
