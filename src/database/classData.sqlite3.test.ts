d2f1ddc620048f6142f7edda3205f860f9685cf5
HDPancakes <cannor.fitz@gmail.com>
2023-09-27 12:35:05 -0400

    added tests for classDB

--- src/database/classData.sqlite3.test.ts
+++ src/database/classData.sqlite3.test.ts
@@ -3,8 +3,26 @@ import { classDB } from "./classData"
 
 //For more info on writing tests, see <https://bun.sh/docs/test/writing>
 
-describe('Demo Database', () => {
+describe('Class Data', () => {
     it('Should have at least one test',() => {
-        expect(null).not.toBeNull()
+        expect(
+            classDB.query("SELECT * FROM Classes LIMIT 1")
+        ).not.toBeNull()
+    })
+
+    it('Should contain "CSCI362"', () => {
+        expect(
+            classDB.query("select * from Classes where courseName ='CSCI362'").get()
+        ).toMatchObject({
+            CRN: 10155,
+            partOfTerm: 1,
+            section: 1,
+            online: false,
+            independentStudy: false,
+            creditHours: 3,
+            maximumEnrollment: 22,
+            seatsAvailable: -3,
+            courseName: "CSCI362"
+        })
     })
 })
