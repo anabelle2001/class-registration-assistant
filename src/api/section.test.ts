6a84dfde5cd1e3d75a19bfdb6b33d24edc1d4365
Anabelle <git@anabelle.dev>
2023-09-30 18:16:52 -0400

    make tests

--- src/api/section.test.ts
+++ src/api/section.test.ts
@@ -3,16 +3,26 @@ import { sectionAPI } from "./section";
 
 describe('Demo API', () => {
     it("should be neither `null` nor `undefined`", async () => {
-        let response = await sectionAPI.request('/')
+        let response = await sectionAPI.request('/?semesterID=202410')
         let responseText = await response.text()
 
         expect(responseText).not.toBeNil()
     })
 
     it('should respond with JSON', async () => {
-        let response = await sectionAPI.request('/')
+        let response = await sectionAPI.request('/?semesterID=202410')
         let responseJSON = await response.json()
-
+        // console.debug(responseJSON);
+        
         expect(responseJSON).not.toBeNil()
+    })
+
+    it('should respond with an array of at least one item', async () => {
+        let response = await sectionAPI.request('/?semesterID=202410')
+        let responseJSON = await response.json()
+        // console.debug(responseJSON);
+
+        expect(responseJSON).toBeArray()
+        expect(responseJSON).not.toBeArrayOfSize(0)
     })
 })
