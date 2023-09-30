43b6d688825483b9fff2f8d158e3a3a1083a2159
Anabelle <git@anabelle.dev>
2023-09-30 18:16:38 -0400

    change parameter to query

--- src/api/section.ts
+++ src/api/section.ts
@@ -6,13 +6,14 @@ const queryAllClasses = classDB.query("SELECT * FROM section WHERE semesterID = 
 
 export const sectionAPI = new Hono();
 
-sectionAPI.get('/:semesterID', (c) => {
+sectionAPI.get('/', (c) => {
     
     // We shouldn't /need to/ check if :semesterID is valid
     // number. if the user gives an invalid :semesterID, the
     // sql query will return an empty array.
 
-    let semesterID = c.req.param().semesterID;
+
+    let semesterID:string = c.req.queries().semesterID[0]
     let responseJSON = queryAllClasses.all(semesterID);
     return c.json(responseJSON);
 });
