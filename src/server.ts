11734ddddae14c81d666c89c1c908aa1be4866e4
Anabelle <git@anabelle.dev>
2023-09-29 20:58:25 -0400

    route api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
+
 import { demoAPI } from './api/demo'
+import { classAPI } from './api/class'
 
-const app = new Hono()
+const app = new Hono();
 
-//Import the demo API into our application, and have it serve requests directed
-//to /api/demo/
 app.route('/api/demo/',demoAPI);
+app.route('/api/classes/',classAPI);
 
 export default app
