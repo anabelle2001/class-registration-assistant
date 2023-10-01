62135c94353bb761ef648ad96fb341d40bcfa3ec
Anabelle <git@anabelle.dev>
2023-10-01 19:53:23 -0400

    remove unneccesary comment

--- src/api/sections.ts
+++ src/api/sections.ts
@@ -7,16 +7,10 @@ const queryAllClasses = classDB.query("SELECT * FROM section WHERE semesterID = 
 export const sectionsAPI = new Hono();
 
 sectionsAPI.get('', (c) => {
-    
-    // We shouldn't /need to/ check if :semesterID is valid
-    // number. if the user gives an invalid :semesterID, the
-    // sql query will return an empty array.
     try {
-        console.log(c.req.queries());
-        
         let semesterID:string = c.req.queries().semesterID[0]
         let responseJSON = queryAllClasses.all(semesterID);
-        
+
         return c.json(responseJSON);
     } catch (error) {
         c.status(400)
