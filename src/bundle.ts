b82913c35857641e8b72a6543af14222339fc643
Anabelle <git@anabelle.dev>
2023-10-05 16:56:43 -0400

    comments

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,5 +1,6 @@
 import { readdir } from 'node:fs/promises';
 
+//Spawns a subprocess to get the names of all the scripts we need to compile
 let findScriptsSubprocess = Bun.spawn([
     'find',
     './src/scripts',
@@ -7,13 +8,18 @@ let findScriptsSubprocess = Bun.spawn([
     '*.ts',
 ])
 
+/* in this next line, we get the text output of that process (eg: an array of strings, with each string being a path to a file).
+
+The @ts-ignore is because I haven't figured out how to tell typescript that our bun environment supports async in the body of a file.*/
+
+// @ts-ignore
 let sourceFileList = (await new Response(findScriptsSubprocess.stdout).text()).split('\n').filter(str => str != '');
 
 
 let bundlerArgs = [
-    'bun',
+    'bun',//We want to run bun, the program
     'build',
-    '--watch',
+    '--watch',//Recompile the javascript whenever the typescript updates
     '--splitting',
     '--target','browser',
     '--sourcemap',
