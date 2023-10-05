fccb3d71ed3266f77ae641de92f1594fff59e842
Anabelle <git@anabelle.dev>
2023-10-05 16:49:07 -0400

    update sections.ts to use new storage paradigm

--- src/api/sections.ts
+++ src/api/sections.ts
@@ -1,20 +1,9 @@
 import { Hono } from 'hono'
 import { classDB } from '../database/classData'
 
-const queryAllClasses = classDB.query("SELECT * FROM section WHERE semesterID = $1");
-
-
 export const sectionsAPI = new Hono();
 
 sectionsAPI.get('', (c) => {
-    try {
-        let semesterID:string = c.req.queries().semesterID[0]
-        let responseJSON = queryAllClasses.all(semesterID);
-
-        return c.json(responseJSON);
-    } catch (error) {
-        c.status(400)
-        return c.json([])
-    }
+    return c.json(classDB.data)
 });
 
