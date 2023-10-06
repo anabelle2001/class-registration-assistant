88c2c9c3ac759b1f152f03b6d03d8e9964e71630
Anabelle <git@anabelle.dev>
2023-10-05 23:54:33 -0400

    update schema

---
+++ sql/db_init.sql
@@ -0,0 +1,60 @@
+CREATE TABLE semester (
+    SID          INTEGER,
+    semesterName TEXT,
+
+    PRIMARY KEY(SID)
+)
+
+CREATE TABLE section (
+    SID INTEGER NOT NULL,
+    CRN INTEGER NOT NULL,
+    section INTEGER,
+    online INTEGER NOT NULL,
+    independentStudy INTEGER NOT NULL,
+    seatsMaximum INTEGER,
+    seatsAvaliable INTEGER,
+    creditHours INTEGER NOT NULL,
+    courseName TEXT NOT NULL,
+    express INTEGER,
+
+    PRIMARY KEY(SID,CRN)
+)
+
+CREATE TABLE meeting (
+    SID INTEGER,
+    CRN INTEGER,
+
+    days TEXT,
+    starts INTEGER,
+    ends INTEGER,
+    room TEXT,
+    PRIMARY KEY(SID,CRN,days,starts),
+    FOREIGN KEY(SID,CRN) REFERENCES section(SID,CRN)
+)
+
+CREATE TABLE faculty (
+    FID INTEGER NOT NULL UNIQUE,
+    Name TEXT,
+    Email TEXT,
+    PRIMARY KEY(FID)
+) 
+
+CREATE TABLE teaches (
+    SID INTEGER,
+    CRN INTEGER,
+    FID INTEGER,
+
+    FOREIGN KEY(FID) REFERENCES faculty(FID),
+    FOREIGN KEY(SID,CRN) REFERENCES section(SID,CRN)
+)
+
+CREATE TABLE enrollment(
+    SID INTEGER,
+    CRN INTEGER,
+    maxSeats INTEGER,
+    filledSeats INTEGER,
+    atTime INTEGER,
+
+    PRIMARY KEY(SID,CRN,atTime),
+    FOREIGN KEY(SID,CRN) REFERENCES section(SID,CRN)
+)
