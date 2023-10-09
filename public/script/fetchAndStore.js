4968da930eb80a5bbebe53ecdb5f71f04fe49893
Sierra <puwalowskism@g.cofc.edu>
2023-10-08 20:38:58 -0400

    here it is

--- public/script/fetchAndStore.js
+++ public/script/fetchAndStore.js
@@ -2,3 +2,25 @@ let response = await fetch("/api/sections/?semesterID=202410")
 document.storage = await response.json();
 console.log(document.storage);
 
+//This is just a test
+//Delete after
+let data = [{
+    //"id": "1",
+    "semesterID": "Fall 2023"
+  },
+  {
+    //"id": "2",
+    "semesterID": "Spring 2023"
+  }
+];
+
+let test = [{
+    response
+}]
+
+let two = [{
+
+}]
+var selectElement = document.getElementById('testing');
+
+data.map(item => testing.appendChild(new Option(item.semesterID)).cloneNode(true));
