3fbbf6a682d43c49eea3f1ea097f181a995a5626
Anabelle <git@anabelle.dev>
2023-10-18 17:49:28 -0400

    delete typescript implementation

--- src/scrape/simple.ts
+++
@@ -1,7 +0,0 @@
-import { getAuthHeaders } from "./authenticate";
-
-
-let second_url = `https://ssb.cofc.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term=202410&startDatepicker=&endDatepicker=&pageOffset=1&pageMaxSize=10&sortColumn=courseReferenceNumber&sortDirection=asc`
-
-let res = await fetch(second_url,{headers:getAuthHeaders()})
-console.log(await res.text());
