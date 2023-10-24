affc605d79ea12dad81ef61b51d663b010031a9a
HDPancakes <cannor.fitz@gmail.com>
2023-10-24 13:51:27 -0400

    Faculty translated

--- src/database/translateSection.ts
+++ src/database/translateSection.ts
@@ -1,10 +1,10 @@
-import {sectionResponse} from '../scrape/ellucianResponseTypes'
-import {section} from  './sectionTypes'
+import {facultyResponse, sectionResponse} from '../scrape/ellucianResponseTypes'
+import {section, trinaryInt, faculty, schedule} from  './sectionTypes'
 
 export default function translateSection(
     responseToTranslate:sectionResponse
 ): section {
-    return {
+    let convertedSection:section =  {
         CRN: +responseToTranslate.courseReferenceNumber,
         semesterID: +responseToTranslate.term,
         
@@ -12,12 +12,44 @@ export default function translateSection(
         section: +responseToTranslate.sequenceNumber,
         
         creditHours: responseToTranslate.creditHourLow,
-        express: 0, //TODO: FIX ME
+        express: null, //TODO: FIX ME
         faculty: [],//TODO: IMPLEMENT ME
         independentStudy: 0, //TODO: IMPLEMENT ME
-        online: 0, //TODO: IMPLEMENT ME
+        online: null, //TODO: IMPLEMENT ME
         schedule: [], //TODO: IMPLEMENT ME
         seatsAvailable: responseToTranslate.seatsAvailable,
         seatsMaximum: responseToTranslate.maximumEnrollment,
     }
+
+    for(let i = 0; i < responseToTranslate.faculty.length ; i++)
+    {
+        let facultyToTranslate = responseToTranslate.faculty[i]
+
+        let newFacObj:faculty = {
+            id: +facultyToTranslate.bannerId,
+            email: facultyToTranslate.emailAddress,
+            name: facultyToTranslate.displayName,
+            
+        }
+
+        convertedSection.faculty.push(newFacObj)
+    }
+
+    // for(let i = 0; i < responseToTranslate.meetingsFaculty.length ; i++)
+    // {
+    //     let scheduleToTranslate = responseToTranslate.meetingsFaculty[i]
+
+    //     let newSchObj:schedule = {
+    //         begins: +scheduleToTranslate.begins,
+    //         ends: +scheduleToTranslate.ends,
+    //         weekdays: scheduleToTranslate.weekdayList,
+    //         room: scheduleToTranslate.room,
+
+    //     }
+
+    //     convertedSection.schedule.push(newSchObj)
+    // }
+
+
+    return convertedSection
 }
