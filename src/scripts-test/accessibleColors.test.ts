9ddef740001a8befa8942899149b2459d49a2434
Anabelle <git@anabelle.dev>
2023-11-06 23:08:59 -0500

    add testz

---
+++ src/scripts-test/accessibleColors.test.ts
@@ -0,0 +1,21 @@
+//describe accessible colors
+import { describe, expect, it } from "bun:test";
+import { Color } from "../scripts/colors/colors";
+import { ColorTools } from '../scripts/colors/accessibleColor'
+
+describe('accessible colors',async ()=>{
+    it('should generate valid AAA_text pairs for 10000 random colors', function(){
+        for(let i = 0; i < 10000; i++){
+            const randColor = Color.random()
+         
+            const randColorPair = ColorTools.deriveContrastingPair(randColor,'AAA_text');
+         
+            if(randColorPair === undefined){
+                print
+            }
+
+            expect(randColorPair).toBeDefined()
+        }
+    })
+    //it should not fail for 100 random colors
+})
