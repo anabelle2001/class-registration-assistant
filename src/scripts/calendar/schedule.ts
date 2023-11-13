90a6fa1e3405709756e30380377699da75c92139
Anabelle <git@anabelle.dev>
2023-11-12 20:01:13 -0500

    only change color once

--- src/scripts/calendar/schedule.ts
+++ src/scripts/calendar/schedule.ts
@@ -2,15 +2,13 @@ import { Color } from "../colors/colors";
 import { ColorTools } from '../colors/accessibleColor'
 import { computeTextColor,computeBackgroundColor } from "./calendarColors";
 
-setInterval(()=>{
-    document.querySelectorAll('.meeting').forEach(el=>{
-        let ell = el as HTMLElement;
+document.querySelectorAll('.schedule .meeting').forEach(el=>{
+    let ell = el as HTMLElement;
 
-        const foo = Color.random()
-        const {dark,light} = ColorTools.deriveContrastingPair(foo)
+    const foo = Color.random()
+    const {dark,light} = ColorTools.deriveContrastingPair(foo)
 
-        ell.style.color = dark.hex;
-        ell.style.backgroundColor = light.hex+'ff';
-        ell.style.borderLeft = `solid ${dark.hex}`;
-    })
-},1000)
+    ell.style.color = dark.hex;
+    ell.style.backgroundColor = light.hex+'ff';
+    ell.style.borderLeft = `solid ${dark.hex}`;
+})
